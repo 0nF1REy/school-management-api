ed3333198ebd4a6834b4d4cef8c068a9aaaf24b6
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-07-02 13:20:02 -0300

    feat(api): Implement root (/) endpoint to return default API message and prevent 404

--- app.py
+++ app.py
@@ -20,3 +20,7 @@ app = FastAPI(
 app.include_router(alunos_router, tags=["alunos"])
 app.include_router(cursos_router, tags=["cursos"])
 app.include_router(matriculas_router, tags=["matriculas"])
+
+@app.get("/")
+def read_root():
+    return {"message": "API do sistema de gestão escolar em funcionamento"}
